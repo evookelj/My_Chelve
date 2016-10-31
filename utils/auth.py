8e2d0b762b98ab672105d69c5fd7487c1269723c
eVookelj <evukelj@stuy.edu>
2016-10-31 19:39:06 -0400

    Add login functionality

--- utils/auth.py
+++ utils/auth.py
@@ -7,18 +7,36 @@ f = "data/chelve.db"
 db = sqlite3.connect(f)
 c = db.cursor()
 
-def regWrap(user, password):
-    try: #does table already exist?
-        query = ("SELECT * FROM users")
-    except: #if not, first user!
-        query = ("CREATE TABLE users (user TEXT, password TEXT, salt TEXT)")
-        c.execute(query)
-    register(user, password)
+salt = "hi"
+
+def login(user, password):
+    count = 0
+    query = ("SELECT * FROM users WHERE user=?")
+    sel = c.execute(query,(user,));
+    for record in sel:
+        count += 1
+        password = sha1(password+salt).hexdigest()
+        if (password==record[2]):
+            print "User has been logged in"
+        else:
+            print "User login has failed. Invalid password"
+    if count==0:
+        print "Username does not exist"
+        
 
 def register(user, password):
+    try: #does table already exist?
+        c.execute("SELECT * FROM USERS")
+    except: #if not, first user!
+        query = ("CREATE TABLE users (user TEXT, salt TEXT, password TEXT)")
+        c.execute(query)
+    regMain(user, password)
+
+def regMain(user, password):
     if regReqs(user, password):
         query = ("INSERT INTO users VALUES (?, ?, ?)");
-        c.execute(query, (user,password,"hi"));
+        password = sha1(password + salt).hexdigest()
+        c.execute(query, (user,"hi",password));
         print "Account created!"
 
 def regReqs(user, password):
@@ -32,6 +50,12 @@ def regReqs(user, password):
     if duplicate(user):
         print "Username already exists"
         success = False
+    if " " in user or " " in password:
+        print "Spaces not allowed in user or password"
+        success = False
+    if user==password:
+        print "Username and password must be different"
+        success = False
     return success
 
 def duplicate(user):
@@ -41,6 +65,7 @@ def duplicate(user):
         return True
     return False
 
-register("emaVookelj","hello-there")
+register("martians","csTeachers")
+login("martians","sesameStreet")
 db.commit()
 db.close()
