2d6092c86f0d1f725447048ad27a82d7293836b0
EMilborn <emilborn@stuy.edu>
2016-11-02 12:25:49 -0400

    comments and minor changes to auth.py

--- utils/auth.py
+++ utils/auth.py
@@ -2,7 +2,7 @@
 
 from hashlib import sha1
 from sqlite3 import connect
-from random import choice
+from os import urandom
 
 f = "data/chelve.db"
 db = connect(f)
@@ -16,13 +16,13 @@ def login(user, password):
         count += 1
         password = sha1(password+record[1]).hexdigest()
         if (password==record[2]):
-            print "User has been logged in"
+            print "User has been logged in" #debugging purposes, to be removed in final
             return True
         else:
-            print "User login has failed. Invalid password"
+            print "User login has failed. Invalid password" #debugging purposes, to be removed in final
             return False
     if count==0:
-        print "Username does not exist"
+        print "Username does not exist" #debugging purposes, to be removed in final
         return False
 
 def register(user, password):
@@ -30,24 +30,26 @@ def register(user, password):
         c.execute("SELECT * FROM USERS")
     except: #if not, first user!
         query = ("CREATE TABLE users (user TEXT, salt TEXT, password TEXT)")
+        #doesn't actually affect functionality or efficiency, but why create a variable (query) that is only gonna be used once?
+        #why not:
+        #c.execute("CREATE TABLE users (user TEXT, salt TEXT, password TEXT)")
         c.execute(query)
     regMain(user, password)
 
 def regMain(user, password):
     reg = regReqs(user, password)
     if reg:
-        salt = ''.join([choice('abcdefghijklmnopqrstuvwxyz123456789') for _ in range(10)])
-        query = ("INSERT INTO users VALUES (?, ?, ?)");
+        salt = os.urandom(10)
+        query = ("INSERT INTO users VALUES (?, ?, ?)")
         password = sha1(password + salt).hexdigest()
-        c.execute(query, (user,salt,password));
+        c.execute(query, (user, salt, password))
         print "Account created!"
     return reg
         
-
 def regReqs(user, password):
     success = True
     if len(password) < 8 or len(password) > 32:
-        print "Password must be 8-32 characters"
+        print "Password must be 8-32 characters"#this is something we want the user to be able to see
         success = False
     if len(user) < 8 or len(user) > 32:
         print "Username must be 8-32 characters"
@@ -70,7 +72,7 @@ def duplicate(user):
         return True
     return False
 
-def getProfile(user):
+def getProfile(user):#move to stories.py
     userStarted = getStarted(user)
     userContd = getContd(user)
     return [userStarted, userContd]
