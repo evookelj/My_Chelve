ce3fa861acb9897835c52e00c137a8f4de528b6b
Harry Lum <hlum1@stuy.edu>
2016-11-03 12:34:40 -0400

    start getFeed, finish getStory

--- utils/stories.py
+++ utils/stories.py
@@ -1,21 +1,22 @@
 import sqlite3
 
-
+# will be used
 def getFeed():
+	db = sqlite3.connect("data/chelve.db")
+	c = db.cursor()
+	data = c.execute("SELECT title,timestamp,number FROM entries")
 	
+	return 0;
 	
 		
 def getUser():
-	
-	
-	
-	
-def getProfile():
-	
+	return 0;
 
-	
+def getProfile():
+	return 0;
+
 def getContributors(title):
-	
+	return 0;
 
 
 # will be used to print the entire story or most recent contribution
@@ -28,20 +29,22 @@ def getStory(storyTitle,fullStory):
 	c = db.cursor()
 	data = c.execute("SELECT title,number,entry FROM entries")
 	
+	for x in data:
+			if x["title"] == storyTitle:
+				dict["author"] = x["user"]
+				dict["timestamp"] = x["timestamp"]
+				break
+
 	if fullStory:	
 		for x in data:
 			if x["title"] == storyTitle:
 				dict["story"]+= x["entry"]
-				
 
 	else:
 		for x in reversed(data):
 			if x["title"] == storyTitle:
 				dict["story"]+= x["entry"]
-				dict["author"] = x["user"]
-				dict["timestamp"] = x["timestamp"]
 				break
-	
 	return dict
 
 def getStarted(user):
@@ -60,12 +63,10 @@ def getContd(user):
         retArr = record[0]
     return retArr
 
-
 def getProfile(user):
     userStarted = getStarted(user)
     userContd = getContd(user)
     return [userStarted, userContd]
-	
 		
 	
 	
